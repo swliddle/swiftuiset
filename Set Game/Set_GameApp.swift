a45b9d43d9d3d73760ead38e3eb5947d9d43ff01
swliddle <liddle@byu.edu>
2020-09-17 19:06:35 -0600

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Steve Liddle on 9/17/20.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
